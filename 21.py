6240f57f25c555d371772585acbf5601d52a6bc3
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-05-28 12:22:22 -0500

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,41 @@
+"""
+
+int+int<=21
+funcion ganar
+
+
+"""
+from random import shuffle
+
+def baraja():
+    return [(n, p) for n in ['A','2','3','4','5','6','7','8','9','10','J','Q','K'] for p in ['corazones','picas','treboles','diamantes']]
+
+def mezclar(baraja):
+    shuffle(baraja)
+    return baraja
+
+def sacar_carta(baraja):
+    if baraja == []:
+        pass
+    else:
+        print(baraja[0], valor_carta(baraja[0]))
+        sacar_carta(baraja[1:])
+        
+def valor_carta(carta):
+    if carta[0] in ['J','Q','K']:
+        return 10
+    elif carta[0]=='A':
+        return 1
+    elif carta[0] in ['2','3','4','5','6','7','8','9','10']:
+        return int(carta[0])
+
+def valor_mano(mano):
+    if mano == []:
+        return 0
+    else:
+        print(mano[0])
+        return valor_mano(mano[0])+valor_mano(mano[1:])
+
+
+
+print(valor_mano(mezclar(baraja())))
